e6074c8da29d4a029eb7cd160d5f4adabcff50c6
0-BSCode <bryanipsanchez@gmail.com>
2024-03-31 00:35:08 +0800

    Initialize repo

---
+++ lex.py
@@ -0,0 +1,28 @@
+class Lexer:
+    def __init__(self, source):
+        pass
+
+    # Process the next character.
+    def nextChar(self):
+        pass
+
+    # Return the lookahead character.
+    def peek(self):
+        pass
+
+    # Invalid token found, print error message and exit.
+    def abort(self, message):
+        pass
+
+    # Skip whitespace except newlines since newlines
+    # indicate the end of a statement.
+    def skipWhitespace(self):
+        pass
+
+    # Skip comments in the code.
+    def skipComment(self):
+        pass
+
+    # Return the next token.
+    def getToken(self):
+        pass
